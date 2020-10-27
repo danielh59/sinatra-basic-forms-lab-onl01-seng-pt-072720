1c224a78a1a08ba253d02b87b4f96cad13dd1460
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-27 06:36:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,11 +13,12 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    @puppy = Puppy.new(params[:name],params[:breed],params[:age])
+    # @puppy = Puppy.new(params[:name],params[:breed],params[:age])
     # @puppy.age = params[:age]
     # @puppy.name = params[:name]
     # @puppy.breed = params[:breed]
-  erb :create_puppy
+  erb :display_puppy
+  
   end
 
 end
